6e041137586b24c697521a4b26e159bd25d499ea
John <88310061+yeh-john@users.noreply.github.com>
2022-11-22 01:51:54 +0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,3 @@
+document.querySelector('.Votes_tradingViewWrapper__awkS1').style.height = '10px';
+document.querySelector('.tradingview-widget-container').style.height = '10px';
+document.querySelector('.tradingview-widget-container').style.display = 'none';
